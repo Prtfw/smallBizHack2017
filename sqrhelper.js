47b077f507ceca5036770cdf0b5d2cd78ed6fe07
Prtfw <shang.madeleine@gmail.com>
2017-12-03 10:40:29 -0500

    sqrApi working

---
+++ sqrhelper.js
@@ -0,0 +1,35 @@
+const request = require("request");
+const url = "https://connect.squareup.com/v2/locations/CBASEIqTooINQnShc9Y1kEhwJcEgAQ/checkouts";
+const requestData = {
+  redirect_url: "http://yahoo.ca",
+  idempotency_key: "xxx7777x",
+  ask_for_shipping_address: false,
+  merchant_support_email: "mns_catdogmice@yahoo.ca",
+
+  order: {
+    reference_id: "{222",
+    line_items: [
+      {
+        name: "flooring",
+        quantity: "2",
+        base_price_money: {
+          amount: 100000,
+          currency: "USD",
+        },
+      },
+    ],
+  },
+  pre_populate_buyer_email: "mns_catdogmice@yahoo.ca",
+};
+
+request(
+  {
+    url: url,
+    method: "POST",
+    json: requestData,
+  },
+  function(err, resp, body) {
+    if (err) console.log("err", err);
+    if (body) console.log("bd", body);
+  },
+);
