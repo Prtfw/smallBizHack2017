d3b442950be9da04d2faa73c45e355c86ca94ab4
Alex Stroulger <alexstroulger@gmail.com>
2017-12-03 00:53:08 -0500

    Tab Navigation

---
+++ Components/Screens/FormList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+class FormList extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>{"heyoooo"}</Text>
+      </View>
+    );
+  }
+}
+
+export default FormList;
